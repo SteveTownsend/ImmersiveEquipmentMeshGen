fb28548d6faf6e2010132de019c9c9f6c8516174
SteveTownsend <SteveTownsend0@gmail.com>
2022-01-19 23:12:09 -0500

    Reinstate AoplyTransform, which fixes many edge cases

--- ImmersiveEquipmentMeshGen/NifTransformer.cs
+++ ImmersiveEquipmentMeshGen/NifTransformer.cs
@@ -266,8 +266,7 @@ namespace ImmersiveEquipmentDisplay
                     return;
 
                 // TODO it appears these functions could be combined. Stick with script flow for safety, at least initially.
-                // per SlavicPotato1 - transform not required, see https://github.com/SlavicPotato/ied-dev/issues/1
-                // ApplyTransform(newId, blockDest); //In case things are at an angle where flipping x would produce incorrect results.
+                ApplyTransform(newId, blockDest); //In case things are at an angle where flipping Z would produce incorrect results.
                 FlipAlongZ(newId, blockDest);
             }
             else
